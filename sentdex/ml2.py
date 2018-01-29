a4ecb5757cf93344ac14c05db96fc5c2bed172e6
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-29 16:10:42 +0530

    NLTK Python3.6

--- sentdex/ml2.py
+++ sentdex/ml2.py
@@ -1,15 +1,13 @@
 import pandas as pd
-import quandl
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL")
-
-print(df.head())
-
+#print(df.head())
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
-
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
-
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
-
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 print(df.head())
